16ad975dd1597678d88821a0c861221ef0b80508
ZhaoKe <1355449245@qq.com>
2024-05-10 15:26:18 +0800

    Add covid19-cough explore

--- readers/audio.py
+++ readers/audio.py
@@ -776,3 +776,48 @@ def make_pad_mask(lengths: torch.Tensor, max_len: int = 0) -> torch.Tensor:
     seq_length_expand = lengths.unsqueeze(-1)
     mask = seq_range_expand >= seq_length_expand
     return mask
+
+
+def segment_cough(x, fs, cough_padding=0.2, min_cough_len=0.2, th_l_multiplier=0.1, th_h_multiplier=2):
+    # Preprocess the data by segmenting each file into individual coughs using a hysteresis comparator on the signal power
+    cough_mask = np.array([False] * len(x))
+
+    # Define hysteresis thresholds
+    rms = np.sqrt(np.mean(np.square(x)))
+    seg_th_l = th_l_multiplier * rms
+    seg_th_h = th_h_multiplier * rms
+
+    # Segment coughs
+    coughSegments = []
+    padding = round(fs * cough_padding)
+    min_cough_samples = round(fs * min_cough_len)
+    cough_start = 0
+    cough_end = 0
+    cough_in_progress = False
+    tolerance = round(0.01 * fs)
+    below_th_counter = 0
+
+    for i, sample in enumerate(x ** 2):
+        if cough_in_progress:
+            if sample < seg_th_l:
+                below_th_counter += 1
+                if below_th_counter > tolerance:
+                    cough_end = i + padding if (i + padding < len(x)) else len(x) - 1
+                    cough_in_progress = False
+                    if (cough_end + 1 - cough_start - 2 * padding > min_cough_samples):
+                        coughSegments.append(x[cough_start:cough_end + 1])
+                        cough_mask[cough_start:cough_end + 1] = True
+            elif i == (len(x) - 1):
+                cough_end = i
+                cough_in_progress = False
+                if (cough_end + 1 - cough_start - 2 * padding > min_cough_samples):
+                    coughSegments.append(x[cough_start:cough_end + 1])
+            else:
+                below_th_counter = 0
+        else:
+            if sample > seg_th_h:
+                cough_start = i - padding if (i - padding >= 0) else 0
+                cough_in_progress = True
+
+    return coughSegments, cough_mask
+
