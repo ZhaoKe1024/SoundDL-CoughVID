efb901e3c4d8b3068c425e847bf614eba818160a
ZhaoKe <1355449245@qq.com>
2024-11-15 11:32:35 +0800

    finish the model and training.

--- SED_CRNN.py
+++ SED_CRNN.py
@@ -60,9 +60,32 @@ from tqdm import tqdm
 import librosa
 import torch
 from torch import nn
-from readers.featurizer import Wave2Mel
+from torch.utils.data import Dataset, DataLoader
+import torchaudio
+
+# from readers.featurizer import Wave2Mel
 
 ROOT = "F:/DATAS/NEUCOUGHDATA_COUGHSINGLE/"
+
+
+class Wave2Mel(object):
+    def __init__(self, sr,
+                 n_fft=1024,
+                 n_mels=128,
+                 win_length=1024,
+                 hop_length=512,
+                 power=2.0
+                 ):
+        self.mel_transform = torchaudio.transforms.MelSpectrogram(sample_rate=sr,
+                                                                  win_length=win_length,
+                                                                  hop_length=hop_length,
+                                                                  n_fft=n_fft,
+                                                                  n_mels=n_mels,
+                                                                  power=power)
+        self.amplitude_to_db = torchaudio.transforms.AmplitudeToDB(stype='power')
+
+    def __call__(self, x):
+        return self.amplitude_to_db(self.mel_transform(x))
 
 
 def min2sec(t: str):
@@ -113,12 +136,15 @@ def labelling_by_IOU(st1, en1, st2, d2, mode="left"):
     else:
         raise Exception("Unknown mode of labelling_by_IOU(mode=\"???\")")
 
+
 def wav_crop_padding(mel, fixed_length=16):
+    # print(mel.shape)
     dim, length = mel.shape
     if length < 16:
         new_mel = np.zeros((128, 16))
         st = np.random.randint(0, (fixed_length - length + 1) // 2)
-        new_mel[:, st:st + 16] = mel
+        new_mel[:, st:st + length] = mel
+        new_mel = torch.from_numpy(new_mel)
     elif length > 16:
         st = np.random.randint(0, (length + 1 - fixed_length) // 2)
         new_mel = mel[:, st:st + 16]
@@ -126,46 +152,67 @@ def wav_crop_padding(mel, fixed_length=16):
         new_mel = mel
     return new_mel
 
+
 def read_metafile(w2m):
     slice_raw = pd.read_csv(ROOT + 'neucough_metainfo_slice.txt', header=0, index_col=0)
     lab2name = {0: "non-cough", 1: "cough"}
     fixed_length = 16
-
-    for idx, item in enumerate(slice_raw.itertuples()):
+    data_list = []
+    label_list = []
+    for idx, item in tqdm(enumerate(slice_raw.itertuples()), desc="ReadFiles:"):
         name, st, en = item[0], item[1], item[2]
         st, en = min2sec(st), min2sec(en)
-        print("st, en:", st, en)
+        # print("st, en:", st, en)
         audio_path = "F:/DATAS/NEUCOUGHDATA_FULL/{}_audiodata_元音字母a.wav".format(name)
         pos_sample, sr = read_audio(audio_path, st=st, en=en)
         pos_mel = w2m(torch.from_numpy(pos_sample).to(torch.float32))
         # 有一个很大的问题，正样本都是填充0更敏感，负样本却是随机截取的更鲁棒，要想办法也把正样本也调整st, en来随机截取
         pos_mel = wav_crop_padding(pos_mel, fixed_length=fixed_length)
-        print("pos_sample:", pos_sample.shape, pos_mel.shape)
-        print("label:", 1, lab2name[1])
+        # print("pos_sample:", pos_sample.shape, pos_mel.shape)
+        # print("label:", 1, lab2name[1])
+        data_list.append(pos_mel)
+        label_list.append(1)
 
         mel16_rand_wavlen = np.random.randint(7680, 8192)  # Mel length: 16
-        print("wavlen:", mel16_rand_wavlen / sr)
+        # print("wavlen:", mel16_rand_wavlen / sr)
         rand_start = get_rand_start(sec=st, wav_length=mel16_rand_wavlen, mode="left", full_length=None, sr=22050)
-        print("rand_start:", rand_start)
+        # print("rand_start:", rand_start)
         neg_sample, _ = read_audio(audio_path, st=rand_start, duration=mel16_rand_wavlen / sr)  # 除法/不要写成//，会导致算什么都是0
         neg_mel = w2m(torch.from_numpy(neg_sample).to(torch.float32))
-        print("neg_sample:", neg_sample.shape, neg_mel.shape)
+        # print("neg_sample:", neg_sample.shape, neg_mel.shape)
         gen_label = labelling_by_IOU(st1=st, en1=en, st2=rand_start, d2=mel16_rand_wavlen / sr)
-        print("label:", gen_label, lab2name[gen_label])
+        # print("label:", gen_label, lab2name[gen_label])
+        data_list.append(neg_mel)
+        label_list.append(gen_label)
+
         while gen_label == 1:
             mel16_rand_wavlen = np.random.randint(7680, 8192)  # Mel length: 16
-            print("wavlen:", mel16_rand_wavlen / sr)
+            # print("wavlen:", mel16_rand_wavlen / sr)
             rand_start = get_rand_start(sec=st, wav_length=mel16_rand_wavlen, mode="left", full_length=None, sr=22050)
-            print("rand_start:", rand_start)
+            # print("rand_start:", rand_start)
             neg_sample, _ = read_audio(audio_path, st=rand_start,
                                        duration=mel16_rand_wavlen / sr)  # 除法/不要写成//，会导致算什么都是0
-            print("neg_sample:", neg_sample.shape)
+            # print("neg_sample:", neg_sample.shape)
             gen_label = labelling_by_IOU(st1=st, en1=en, st2=rand_start, d2=mel16_rand_wavlen / sr)
-            print("label:", gen_label, lab2name[gen_label])
-        print()
-        if idx > 2:
-            print("idx:", idx, mel16_rand_wavlen)
-            break
+            # print("label:", gen_label, lab2name[gen_label])
+            data_list.append(neg_sample)
+            label_list.append(gen_label)
+        # print()
+        # if idx > 3:
+        #     print("idx:", idx, mel16_rand_wavlen)
+        #     break
+    return data_list, label_list
+
+
+class NEUCOUGHDataset(Dataset):
+    def __init__(self, sample_list, label_list):
+        self.sample_list, self.label_list = sample_list, label_list
+
+    def __getitem__(self, ind):
+        return self.sample_list[ind].to(torch.float32), self.label_list[ind]
+
+    def __len__(self):
+        return len(self.label_list)
 
 
 class CRNN(nn.Module):
@@ -174,7 +221,7 @@ class CRNN(nn.Module):
         pool_size = params["pool_size"]
         dropout_rate = params["dropout_rate"]
         nn_cnn2d_filt = params["nb_cnn2d_filt"]
-        rnn_size = params["rnn_size"]
+        # rnn_size = params["rnn_size"]
         fnn_size = params["fnn_size"]
         inp, oup = 1, nn_cnn2d_filt
         self.feature_extractor = nn.Sequential()
@@ -212,13 +259,15 @@ class CRNN(nn.Module):
 
     def forward(self, x_mel):
         # out = self.feature_extractor(x_mel)
+        x_mel = x_mel.unsqueeze(1).transpose(2, 3)  # [16, 1, 128, 16]
         bs = x_mel.shape
-        print(bs)
+        print("forward batch_size:", bs)
         for layer in self.feature_extractor:
             out = layer(x_mel)
             print("CNN layer output:", out.shape)
             x_mel = out
-        x_mel = x_mel.squeeze().transpose(1, 2).reshape(bs[0], bs[2], -1)
+        # x_mel, _ = x_mel.max(dim=-1)
+        x_mel = x_mel.transpose(1, 2).reshape(bs[0], bs[2], -1)
         print("shape of CNN output:", x_mel.shape)
         hidden = self.init_hidden(batch_size=bs[0])
         out, hidden = self.rnn_module(x_mel, hidden)  # out: tensor of shape (batch_size, seq_length, hidden_size)
@@ -230,22 +279,97 @@ class CRNN(nn.Module):
     def init_hidden(self, batch_size):
         # 这个函数写在这里，有一定迷惑性，这个不是模型的一部分，是每次第一个向量没有上下文，在这里捞一个上下文，仅此而已。
         hidden = torch.autograd.Variable(
-            torch.zeros(2 * self.direction, batch_size, self.hidden_size, device='cpu'))
+            torch.zeros(2 * self.direction, batch_size, self.hidden_size, device='cuda'))
         return hidden
 
 
 def main():
+    wav2mel = Wave2Mel(sr=22050)
+    sample_list, label_list = read_metafile(w2m=wav2mel)
+    split_pos = int(len(label_list) * 0.9)
+    train_dataset = NEUCOUGHDataset(sample_list[:split_pos], label_list[:split_pos])
+    train_loader = DataLoader(dataset=train_dataset, batch_size=16, shuffle=True,
+                              pin_memory=True,
+                              num_workers=0)
+    test_dataset = NEUCOUGHDataset(sample_list[split_pos:], label_list[split_pos:])
+    test_loader = DataLoader(dataset=test_dataset, batch_size=16, shuffle=False,
+                             pin_memory=True,
+                             num_workers=0)
+
     # pool_size = [8, 8, 2]
     params = {"pool_size": [8, 8, 2], "dropout_rate": 0.0, "batch_size": 32, "nb_cnn2d_filt": 64,
               "rnn_size": [128, 128], "fnn_size": [256, 128, 32]}
-    crnn_model = CRNN(params=params)
-    print(crnn_model)
-    x = torch.rand(size=(32, 1, 16, 128))
-    crnn_model(x)
-    # # loss_f1 = nn.CrossEntropyLoss()
-    # # loss_f2 = nn.MSELoss()
-    # # optim = torch.optim.Adam()
-    # print(crnn_model)
+    device = torch.device("cuda")
+    model = CRNN(params=params).to(device)
+    print(model)
+    # x = torch.rand(size=(32, 1, 16, 128))
+    # crnn_model(x)
+    loss_f1 = nn.CrossEntropyLoss().to(device)
+    # loss_f2 = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.02, betas=(0.5, 0.999))
+
+    for epoch_id in range(10):
+        # print()
+        model.train()
+        for idx, (x_input, y_label) in tqdm(enumerate(train_loader), desc=">>Epoch[{}]:".format(epoch_id)):
+            x_input = x_input.to(device)
+            y_label = y_label.to(device)
+            optimizer.zero_grad()
+            y_pred, _ = model(x_input)
+            loss_v = loss_f1(input=y_label, target=y_pred)
+            loss_v.backward()
+            optimizer.step()
+
+        model.eval()
+        y_preds = None
+        y_labels = None
+        for idx, (x_input, y_label) in tqdm(enumerate(train_loader), desc=">>Valid Train:"):
+            x_input = x_input.to(device)
+            y_label = y_label.to(device)
+            if y_labels is None:
+                y_labels = y_label
+            else:
+                y_labels = torch.concat((y_labels, y_label), dim=0)
+
+            with torch.no_grad():
+                y_pred, _ = model(x_input)
+            if y_preds is None:
+                y_preds = y_pred
+            else:
+                y_preds = torch.concat((y_preds, y_pred), dim=0)
+        from sklearn import metrics
+        y_labs = y_labels.data.cpu().numpy()
+        y_preds = y_preds.data.cpu().numpy()
+        y_preds_label = y_preds.argmax(-1)
+        precision = metrics.precision_score(y_labs, y_preds_label)
+        recall = metrics.recall_score(y_labs, y_preds_label)
+        acc = metrics.accuracy_score(y_labs, y_preds_label)
+        print(precision, recall, acc)
+
+        y_preds = None
+        y_labels = None
+        for idx, (x_input, y_label) in tqdm(enumerate(test_loader), desc=">>Valid Test:"):
+            x_input = x_input.to(device)
+            y_label = y_label.to(device)
+            if y_labels is None:
+                y_labels = y_label
+            else:
+                y_labels = torch.concat((y_labels, y_label), dim=0)
+
+            with torch.no_grad():
+                y_pred, _ = model(x_input)
+            if y_preds is None:
+                y_preds = y_pred
+            else:
+                y_preds = torch.concat((y_preds, y_pred), dim=0)
+        from sklearn import metrics
+        y_labs = y_labels.data.cpu().numpy()
+        y_preds = y_preds.data.cpu().numpy()
+        y_preds_label = y_preds.argmax(-1)
+        precision = metrics.precision_score(y_labs, y_preds_label)
+        recall = metrics.recall_score(y_labs, y_preds_label)
+        acc = metrics.accuracy_score(y_labs, y_preds_label)
+        print(precision, recall, acc)
 
 
 if __name__ == '__main__':
