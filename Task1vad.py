c87e893bc3e8c216fb9f01cefe047eb89de4c41a
ZhaoKe <1355449245@qq.com>
2025-01-02 21:39:57 +0800

    update model design for vad task.

---
+++ Task1vad.py
@@ -0,0 +1,165 @@
+#!/user/zhao/miniconda3/envs/torch-0
+# -*- coding: utf_8 -*-
+# @Time : 2025/1/2 20:29
+# @Author: ZhaoKe
+# @File : Task1vad.py
+# @Software: PyCharm
+import os
+import numpy as np
+import pandas as pd
+import librosa
+
+
+# newdf.groupby("binlab").count()
+def min2sec(t: str):
+    parts = t.split(':')
+    res = float(parts[-1])
+    f = 60
+    for i in range(len(parts) - 1):
+        res += int(parts[len(parts) - 2 - i]) * f
+        f *= 60
+    return res
+
+
+def load_bilicough_dataset():
+    ROOT = "G:/DATAS-Medical/BILIBILICOUGH/"
+    metadf = pd.read_csv(ROOT + "bilicough_metainfo.csv", delimiter=',', header=0, index_col=None, usecols=[0, 1, 2, 5],
+                         encoding="ansi")
+    print(metadf)
+    cur_fname = None
+    cur_wav = None
+    data_length = None
+    sample_list = []
+    label_list = []
+    sr_list = []
+    sr = None
+    pre_st, pre_en = None, None
+    # filename	st	en	labelfull	labelname	label	binlab
+    for ind, item in enumerate(metadf.itertuples()):
+        if (cur_fname != item[1]) or (cur_fname is None):
+            cur_fname = item[1]
+            cur_wav, sr = librosa.load(ROOT + cur_fname + ".wav")
+            if sr not in sr_list:
+                sr_list.append(sr)
+            data_length = int(sr)
+        st, en = int(min2sec(item[2]) * sr), int(min2sec(item[3]) * sr + 1)
+        if en > len(cur_wav):
+            en = len(cur_wav)
+        if en - st < 100:
+            raise Exception("Error Index.")
+        sn = en - st
+        # sec = (en - st)/22050
+        if (pre_en is None):
+            if st >= data_length:
+                st_pos = 0
+                ind = 0
+                while st_pos + data_length <= st:
+                    # if len(cur_wav[st_pos:st_pos+data_length]) != sr:
+                    #     raise Exception("Error Length.")
+                    sample_list.append(cur_wav[st_pos:st_pos + data_length])
+                    label_list.append(0)
+                    st_pos += data_length
+                    ind += 1
+                    if ind > 2:
+                        break
+                sample_list.append(cur_wav[st - data_length:st])
+                label_list.append(0)
+        else:
+            if st - pre_en >= sr:
+                st_pos = pre_en
+                ind = 0
+                while st_pos + data_length <= st:
+                    # if len(cur_wav[st_pos:st_pos+data_length]) != sr:
+                    #     raise Exception("Error Length.")
+                    sample_list.append(cur_wav[st_pos:st_pos + data_length])
+                    label_list.append(0)
+                    st_pos += data_length
+                    ind += 1
+                    if ind > 2:
+                        break
+                sample_list.append(cur_wav[st - data_length:st])
+                label_list.append(0)
+        label = int(item[4])
+        if sn == data_length:
+            # if len(cur_wav[st:en]) != sr:
+            #     raise Exception("Error Length.")
+            sample_list.append(cur_wav[st:en])
+            if label in [6, 7]:
+                label_list.append(0)
+            else:
+                label_list.append(1)
+        elif sn < data_length:
+            new_sample = np.zeros(data_length)
+            # print(st, en, sn, len(cur_wav), item[1])
+            if en <= len(cur_wav):
+                new_sample[:sn] = cur_wav[st:en]
+            else:
+                new_sample[:sn] = cur_wav[len(cur_wav) - sn:len(cur_wav)]
+            # if len(new_sample) != sr:
+            #     raise Exception("Error Length.")
+            sample_list.append(new_sample)
+            if label in [6, 7]:
+                label_list.append(0)
+            else:
+                label_list.append(1)
+        else:
+            cnt_sum = sn // data_length + 1
+            res = cnt_sum * data_length - sn
+            overlap = res // (cnt_sum - 1)
+            st_pos = st
+            while st_pos + data_length < en:
+                # if len(cur_wav[st_pos:st_pos+data_length]) < data_length:
+                #     tmp_length = len(cur_wav[st_pos:st_pos+data_length])
+                #     print(data_length, tmp_length)
+                #     # raise Exception("Error Length.")
+                #     print("Error Length.")
+                #     new_sample = np.zeros(data_length)
+                #     new_sample[:tmp_length] = cur_wav[st_pos:st_pos+data_length]
+                #     sample_list.append(new_sample)
+                # else:
+                #     sample_list.append(cur_wav[st_pos:st_pos+data_length])
+                sample_list.append(cur_wav[st_pos:st_pos + data_length])
+                if label in [6, 7]:
+                    label_list.append(0)
+                else:
+                    label_list.append(1)
+                st_pos += data_length - overlap
+            sample_list.append(cur_wav[en - data_length:en])
+            label_list.append(1)
+        pre_st, pre_en = st, en
+    print("sound count:{}, all count:{}.".format(sum(label_list), len(label_list)))
+    print(sr_list)
+    return sample_list, label_list
+
+
+def load_bilinoise_dataset():
+    NOISE_ROOT = "G:/DATAS-Medical/BILINOISE/"
+    noise_length = None
+    filter_length = 25
+    ind = 0
+    new_noise_list = []
+    for item in os.listdir(NOISE_ROOT):
+        if item[-4:] == ".wav" and len(item) >= filter_length:
+            cur_fname = NOISE_ROOT + item
+            cur_wav, sr = librosa.load(cur_fname)
+            noise_length = int(sr)
+            L = len(cur_wav)
+            st_pos = np.random.randint(0, L - noise_length)
+            new_noise_list.append(cur_wav[st_pos:st_pos + noise_length])
+            # print(NOISE_ROOT+item)
+        ind += 1
+        if ind > 18:
+            break
+    for item in new_noise_list:
+        print(len(item))
+
+
+
+
+if __name__ == '__main__':
+    sample_list, label_list = load_bilicough_dataset()
+    length_list = []
+    for item in sample_list:
+        if len(item) not in length_list:
+            length_list.append(len(item))
+    print(length_list)
--- test_code.py
+++ test_code.py
@@ -8,6 +8,10 @@
 暂时测试代码用
 """
 
+import torch
+import torch.nn as nn
+import torchaudio
+
 
 def redundancy_overlap_generate():
     length = 46
@@ -34,6 +38,94 @@ def min2sec(t: str):
     return res
 
 
+class Wave2Mel(object):
+    def __init__(self, sr,
+                 n_fft=1024,
+                 n_mels=128,
+                 win_length=1024,
+                 hop_length=512,
+                 power=2.0
+                 ):
+        self.mel_transform = torchaudio.transforms.MelSpectrogram(sample_rate=sr,
+                                                                  win_length=win_length,
+                                                                  hop_length=hop_length,
+                                                                  n_fft=n_fft,
+                                                                  n_mels=n_mels,
+                                                                  power=power)
+        self.amplitude_to_db = torchaudio.transforms.AmplitudeToDB(stype='power')
+
+    def __call__(self, x):
+        return self.amplitude_to_db(self.mel_transform(x))
+
+
+def find_wavlength():
+    w2m = Wave2Mel(sr=22050)
+    for length in range(22050, 33075, 1000):
+        print(w2m(torch.rand(4, length)).shape)
+
+
+def model_design_task1():
+    class TDNN(nn.Module):
+        def __init__(self, window=1024, overlap=768):
+            super(TDNN, self).__init__()
+            self.tdnn = nn.Sequential(
+                nn.Conv1d(1, 32, kernel_size=window, stride=window - overlap, padding=0),
+                nn.ReLU(),
+                nn.MaxPool1d(kernel_size=4, stride=2),
+                nn.Conv1d(32, 64, kernel_size=5, stride=1, padding=2),
+                nn.ReLU(),
+                nn.MaxPool1d(kernel_size=2, stride=2)
+            )
+
+        def forward(self, x):
+            return self.tdnn(x)
+
+    class CNN(nn.Module):
+        def __init__(self):
+            super(CNN, self).__init__()
+            self.cnn = nn.Sequential(
+                nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2)
+            )
+
+        def forward(self, x):
+            return self.cnn(x)
+
+    class AudioClassifier(nn.Module):
+        def __init__(self):
+            super(AudioClassifier, self).__init__()
+            self.tdnn = TDNN()
+            self.cnn = CNN()
+            self.fc = nn.Sequential(
+                nn.Linear(64 * 55 + 64 * 11 * 11, 128),
+                nn.ReLU(),
+                nn.Linear(128, 2)
+            )
+
+        def forward(self, waveform, melspectrogram):
+            tdnn_out = self.tdnn(waveform)
+            cnn_out = self.cnn(melspectrogram)
+            tdnn_out = tdnn_out.view(tdnn_out.size(0), -1)
+            cnn_out = cnn_out.view(cnn_out.size(0), -1)
+            combined = torch.cat((tdnn_out, cnn_out), dim=1)
+            return self.fc(combined)
+
+    tdnn_module = TDNN(window=1024, overlap=768)
+    cnn_module = CNN()
+    cls_module = AudioClassifier()
+
+    x_wav = torch.rand(size=(32, 22050))
+    x_mel = torch.rand(size=(32, 128, 44))
+    print(tdnn_module(x_wav.unsqueeze(1)).shape)
+    print(cnn_module(x_mel.unsqueeze(1)).shape)
+
+
 if __name__ == '__main__':
     # redundancy_overlap_generate()
-    print(min2sec("00:01:19.06")*22050)
+    # print(min2sec("00:01:19.06")*22050)
+    # find_wavlength()
+    model_design_task1()
